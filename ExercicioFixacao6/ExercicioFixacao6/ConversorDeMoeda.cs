e6f6c4a4224883c4a09f4939dfda3c1199d5141e
Douglas Samuel Gonçalves <douglassam007@gmail.com>
2021-12-21 14:03:09 -0300

    ExercicioFixacao6

---
+++ ExercicioFixacao6/ExercicioFixacao6/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercicioFixacao6
+{
+    class ConversorDeMoeda
+    {
+        public static double iof = 1.06;
+
+        public static double Conversor(double cotacao, double quantidade)
+        {
+            return cotacao * quantidade * iof;
+        }
+    }
+}
---
+++ ExercicioFixacao6/ExercicioFixacao6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cot, qtd;
+
+            Console.Write("Qual é a cotação do dólar? ");
+
+            cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+
+            qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write($"Valor a ser pago em reais = {ConversorDeMoeda.Conversor(cot,qtd).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
