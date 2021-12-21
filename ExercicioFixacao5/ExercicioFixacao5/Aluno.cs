d03bfaca7fb0efdde55ea4a0dc7bb8811ed5a749
Douglas Samuel Gonçalves <douglassam007@gmail.com>
2021-12-21 13:32:43 -0300

    ExercicioFixacao5

---
+++ ExercicioFixacao5/ExercicioFixacao5/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExercicioFixacao5
+{
+    class Aluno
+    {
+        public string Nome;
+        public double[] Notas = {0, 0, 0};
+        public double NotaFinal()
+        {
+            return Notas[0] + Notas[1] + Notas[2];
+        }
+        public string Situacao()
+        {
+            if(NotaFinal() >= 60)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO";
+            }
+        }
+        public double DiferencaNotaCorte()
+        {
+            return 60 - NotaFinal();
+        }
+    }
+}
---
+++ ExercicioFixacao5/ExercicioFixacao5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+
+            for (int i = 0; i < 3; i++)
+            {
+                aluno.Notas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine($"NOTA FINAL = {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine(aluno.Situacao());
+            if(aluno.Situacao() == "REPROVADO")
+            {
+                Console.WriteLine($"FALTARAM {aluno.DiferencaNotaCorte().ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+        }
+    }
+}
