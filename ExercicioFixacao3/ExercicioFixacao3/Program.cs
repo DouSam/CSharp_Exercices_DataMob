8f474f42a8b4e3bee81e12b53c0e5eae0e0bde40
Douglas Samuel Gonçalves <douglassam007@gmail.com>
2021-12-21 12:04:09 -0300

    ExercicioFixacao3 - Classes e Atributos.

---
+++ ExercicioFixacao3/ExercicioFixacao3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            Retangulo retangulo = new Retangulo();
+
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"AREA = {retangulo.Area().ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {retangulo.Diagonal().ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
