930c890bcde4d499af3ab06135f6dfaf7a1f51f0
Douglas Samuel Gonçalves <douglassam007@gmail.com>
2021-12-23 09:06:09 -0300

    ExercicioFixacao9

---
+++ ExercicioFixacao9/ExercicioFixacao9/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioFixacao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int auxId;
+            List<Funcionario> funcionarios = new List<Funcionario>();
+            Funcionario auxFunc;
+            Console.Write("Quantos empregados serão registrados? ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string auxNome;
+                double auxSalario;
+
+                Console.WriteLine($"\nEmpregado #{i + 1}: ");
+
+                do
+                {
+                    Console.Write("Id: ");
+                    auxId = int.Parse(Console.ReadLine());
+                    auxFunc = funcionarios.Find(x => x.Id == auxId);
+                    if (auxFunc != null)
+                    {
+                        Console.WriteLine("ID já cadastrado.");
+                    }
+                } while (auxFunc != null);
+
+                Console.Write("Nome: ");
+                auxNome = Console.ReadLine();
+
+                Console.Write("Salario: ");
+                auxSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionarios.Add(new Funcionario(auxId,auxNome,auxSalario));
+            }
+
+            Console.Write("\nInsira o ID do funcio que terá o salário aumentado: ");
+            auxId = int.Parse(Console.ReadLine());
+            auxFunc = funcionarios.Find(x => x.Id == auxId);
+            if (auxFunc != null)
+            {
+                double perc;
+                Console.Write("Qual o percentual: ");
+                perc = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                auxFunc.Aumento(perc);
+            }
+            else
+            {
+                Console.WriteLine("O ID inserido não existe.");
+            }
+
+            Console.WriteLine("\nLista atualizada de funcionarios.");
+
+            foreach (Funcionario func in funcionarios)
+            {
+                Console.WriteLine(func);
+            }
+        }
+    }
+}
