5b2dd10bc6ede114bec07d5f6a5d09f7acc4c24b
Douglas Samuel <douglassam007@gmail.com>
2021-12-28 13:40:23 -0300

    Initial commit chess game.

---
+++ Chess-Console/Chess-Console/Board/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Board
+{
+    class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+}
---
+++ Chess-Console/Chess-Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using Board;
+using System;
+
+namespace Chess_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p;
+
+            p = new Position(3,4);
+
+        }
+    }
+}
